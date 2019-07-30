f4be1c50a03b7193288fbfcc7461e7180eadfb35
JacksonChen <JackonChen@example.com>
2019-07-30 21:07:41 +0800

    This is an apple

---
+++ src/main/java/cn/jackson/rbac_shiro/RbacShiroApplication.java
@@ -0,0 +1,13 @@
+package cn.jackson.rbac_shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbacShiroApplication.class, args);
+    }
+
+}
---
+++ src/main/java/cn/jackson/rbac_shiro/config/CustomeRealm.java
@@ -0,0 +1,4 @@
+package cn.jackson.rbac_shiro.config;
+
+public class CustomeRealm {
+}
